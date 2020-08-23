c92c427751a669934f5e3bc187da9c5349c75d88
nas <hts.ns.b23@gmail.com>
2020-08-23 12:57:25 +0000

    logout link

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,7 +16,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    flash[:success] ="ログアウト"
+    flash[:success] ="ログアウト完了"
     redirect_to root_url
   end
   
